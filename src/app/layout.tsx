5074c06167eef0a29e39cf2ac39beca7533cc2b7
d3v310p3r.1437 <d3v310p3r.1437@gmail.com>
2026-01-07 03:05:44 +0000

    Сайжруулалт

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ export const metadata: Metadata = {
   title: 'Ayli',
   description: 'Ayli Wi-Fi Access',
   icons: {
-    icon: './ayli-logo.png',
-    shortcut: './ayli-logo.png',
-    apple: './ayli-logo.png',
+    icon: './favico.png',
+    shortcut: './favico.png',
+    apple: './favico.png',
   },
 };
 
